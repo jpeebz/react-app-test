bea09b3a04c81f09aa922b9de0fb02c14124fb07
jamie <peebles.jamie1@gmail.com>
2019-02-14 15:44:47 -0600

    Init commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: "./client/index.js",
+    output: {
+        path: path.join(__dirname, "client"),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /.jsx?/,
+            loader: "babel-loader",
+            exclude: /node_modules/,
+            query: {
+                presets: ["@babel/preset-env", "@babel/preset-react" ]
+            }
+        }, {
+            test: /\.css$/,
+            loader: "style-loader!css-loader"
+        }]
+    },
+};
